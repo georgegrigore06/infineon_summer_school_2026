2b263f852274d3b0f9bd150b087b2ca534b8d0a3
Grigore George <georgegrigore010@gmail.com>
2026-07-13 09:52:29 +0300

    Revert "Finished Digital Design"
    This reverts commit 72617374122dc249961ae37f256679f975ed6de3.

--- source/sv/timer_tb/include/env/ifx_dig_types.svh
+++
@@ -1,3 +0,0 @@
-
-
-typedef enum int {DISABLED = 0, PWM_MODE = 1, COUNTER_MODE = 2, TIMER_MODE = 3} mode_t;
--- source/sv/timer_tb/include/env/registers/ifx_dig_regblock_pkg.sv
+++
@@ -1,8 +0,0 @@
-package ifx_dig_regblock_pkg;
-
-  import uvm_pkg::*;
-  `include "uvm_macros.svh"
-
-  // TODO DAY2: Add include for register model classes here
-
-endpackage
